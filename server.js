2c7b346ca5dada09208435df663505a101ed3907
Andrew McGhie <andrewcmcghie@gmail.com>
2021-02-02 04:53:39 -0500

    routes

--- server.js
+++ server.js
@@ -1,27 +1,60 @@
-// Dependencies
-// =============================================================
-const express = require("express");
-const fs = require("fs");
+var path = require('path');
+var fs = require('fs');
 
+var express = require('express');
+var { uuid } = require('uuidv4');
+var helmet = require('helmet');
 
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-// =============================================================
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/assets", express.static("./assets"));
 
+app.get('/notes', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-require("./routing/api-routes")(app);
-require("./routing/html-routes")(app);
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+//////////////******************* */
+app.get('/api/notes', function (req, res) {
+  fs.readFile(__dirname + '/db/db.json', (err, data) => {
+    if (err) throw err;
+    var db = JSON.parse(data);
+    res.send(db);
+  });
+});
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.post('/api/notes', function (req, res) {
+  let note = { ...req.body, id: uuid() };
+
+  fs.readFile(__dirname + '/db/db.json', (err, data) => {
+    if (err) throw err;
+    var db = JSON.parse(data);
+    db.push(note);
+
+    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(db), (err, data) => {
+      if (err) throw err;
+      res.send(data);
+    });
+  });
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+  fs.readFile('db/db.json', (err, data) => {
+    var db = JSON.parse(data);
+    var savedNote = db.filter((item) => item.id !== req.params.id);
+    fs.writeFile('db/db.json', JSON.stringify(savedNote, null, 2), (err) => {
+      if (err) throw err;
+      res.send(req.body);
+    });
+  });
+});
+
+app.listen(PORT, function () {
+  console.log('App listening on PORT ' + PORT);
 });
